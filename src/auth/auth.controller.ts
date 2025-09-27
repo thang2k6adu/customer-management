1475900bc37bfbeb04a651183f91985bc65fea89
thang2k6adu <thang2k6adu@gmail.com>
2025-09-27 20:35:34 +0700

    feature: implement role guard details: - refactor auth module:   - add dto validate request   - refactor interface: move to other proper destination - implement role guard:   - create Roles custom decorator   - create role guard:     - implement CanActivate interface     - define whether a request have permission to route?     - return true -> request continue access to controller handdler     - false -> stop request & return 403     - canActivate can also return a promise<boolean>

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register(@Body() body: { email: string; password: string; name: string }) {
-    return this.authService.register(body.email, body.password, body.name);
+  register(@Body() registerDto: RegisterDto) {
+    const { email, password, name } = registerDto;
+    return this.authService.register(email, password, name);
   }
 
   @Post('login')
-  login(@Body() body: { email: string; password: string }) {
-    return this.authService.login(body.email, body.password);
+  login(@Body() loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    return this.authService.login(email, password);
   }
 }
