ae027593810b98b44977b6f46011931a17f6aff5
thang2k6adu <thang2k6adu@gmail.com>
2025-09-29 13:32:17 +0700

    feature: custom message in product & dev env

--- src/common/filters/all-exceptions.filter.ts
+++ src/common/filters/all-exceptions.filter.ts
@@ -10,23 +10,26 @@ import { Request, Response } from 'express';
 // Catch này không có tham số nên sẽ bắt mọi lỗi của hệ thống, system, run time,...
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  // exception là lỗi mà controller/service ném ra
-  // host chứa contexxt của request -> truy suất req, res
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const res = ctx.getResponse<Response>();
     const req = ctx.getRequest<Request>();
 
     const traceId = req.traceId || 'unknown';
+    const isDev = process.env.NODE_ENV !== 'production';
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
     let message = 'Internal server error';
 
     if (exception instanceof HttpException) {
       status = exception.getStatus();
-      message = exception.message;
+      message = isDev
+        ? exception.message
+        : 'Có lỗi xảy ra, vui lòng thử lại sau.';
     } else if (exception instanceof Error) {
-      message = exception.message;
+      message = isDev
+        ? exception.message
+        : 'Có lỗi xảy ra, vui lòng thử lại sau.';
     }
 
     res.status(status).json({
